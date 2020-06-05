2e29daa8289903db1359cdc9edfb8a339ddfe546
Evelyn <evelynbr@live.com>
2020-06-05 15:48:11 -0300

    First TDD

--- src/CursoTDDxUnit.Dominio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CursoTDDxUnit.Dominio
-{
-    public class Class1
-    {
-    }
-}
---
+++ test/XUnitTestProject1/Curso/CursoTest.cs
@@ -0,0 +1,44 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DomainTest.Curso
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            //Arrange  
+            var cursoEsperado = new
+            {
+                nome = "",
+                cargaHoraria = (double)80,
+                publicoAlvo = "",
+                valor = (double)950,
+            };
+
+            //Action
+            var curso = new Curso(cursoEsperado.nome, cursoEsperado.cargaHoraria, cursoEsperado.publicoAlvo, cursoEsperado.valor);
+            //Assert
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+    }
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        {
+            this.Nome = nome;
+            this.CargaHoraria = cargaHoraria;
+            this.PublicoAlvo = publicoAlvo;
+            this.Valor = valor;
+        }
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+    }
+}
