73107bef48fc0d37174a7b16542875216f6df8e6
Evelyn <evelynbr@live.com>
2020-06-05 17:44:39 -0300

    test3

--- test/XUnitTestProject1/Curso/CursoTest.cs
+++ test/XUnitTestProject1/Curso/CursoTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 using Xunit;
 
@@ -15,22 +16,53 @@ namespace DomainTest.Curso
             //Arrange  
             var cursoEsperado = new
             {
-                nome = "",
+                nome = "Microsoft Office",
                 cargaHoraria = (double)80,
-                publicoAlvo = "",
-                valor = (double)950,
+                publicoAlvo = PublicoAlvo.Estudante,
+                valor = (double)950
             };
 
             //Action
             var curso = new Curso(cursoEsperado.nome, cursoEsperado.cargaHoraria, cursoEsperado.publicoAlvo, cursoEsperado.valor);
             //Assert
-            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+            Assert.Equal(cursoEsperado.nome, curso.Nome);
+            Assert.Equal(cursoEsperado.cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(cursoEsperado.publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(cursoEsperado.valor, curso.Valor);
         }
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
+        {
+            var cursoEsperado = new
+            {
+                nome = "Informatica basica",
+                cargaHoraria = (double)80,
+                publicoAlvo = PublicoAlvo.Estudante,
+                valor = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(() => 
+                new Curso(nomeInvalido, cursoEsperado.cargaHoraria, cursoEsperado.publicoAlvo, cursoEsperado.valor));
+        }
+    }
+    public enum PublicoAlvo
+    {
+        Estudante, 
+        Universitário,
+        Empregado,
+        Empreendedor
     }
     public class Curso
     {
-        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
         {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentException();
+            }
+
             this.Nome = nome;
             this.CargaHoraria = cargaHoraria;
             this.PublicoAlvo = publicoAlvo;
@@ -38,7 +70,7 @@ namespace DomainTest.Curso
         }
         public string Nome { get; private set; }
         public double CargaHoraria { get; private set; }
-        public string PublicoAlvo { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
         public double Valor { get; private set; }
     }
 }
