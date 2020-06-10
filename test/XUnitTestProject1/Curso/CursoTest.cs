19e622a44651781fa03a37136c81cce38b72fe6a
Evelyn <evelynbr@live.com>
2020-06-10 12:33:11 -0300

    3 mod 12.Setup Cleanup

--- test/XUnitTestProject1/Curso/CursoTest.cs
+++ test/XUnitTestProject1/Curso/CursoTest.cs
@@ -5,11 +5,27 @@ using System.Collections.Generic;
 using System.Net.Http.Headers;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace DomainTest.Curso
 {
     public class CursoTest
     {
+        private readonly ITestOutputHelper _output;
+        private readonly string _nome;
+        private readonly double _cargaHoraria;
+        private readonly PublicoAlvo _publicoAlvo;
+        private readonly double _valor;
+        public CursoTest(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Construtor Executado");
+
+            _nome = "Microsoft Office";
+            _cargaHoraria = 80;
+            _publicoAlvo = PublicoAlvo.Estudante;
+            _valor = 950;
+        }
         [Fact]
         public void DeveCriarCurso()
         {
@@ -35,17 +51,31 @@ namespace DomainTest.Curso
         [InlineData(null)]
         public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
         {
-            var cursoEsperado = new
-            {
-                nome = "Informatica basica",
-                cargaHoraria = (double)80,
-                publicoAlvo = PublicoAlvo.Estudante,
-                valor = (double)950
-            };
-
             Assert.Throws<ArgumentException>(() => 
-                new Curso(nomeInvalido, cursoEsperado.cargaHoraria, cursoEsperado.publicoAlvo, cursoEsperado.valor));
+                new Curso(nomeInvalido, _cargaHoraria, _publicoAlvo, _valor));
         }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmaCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+
+            Assert.Throws<ArgumentException>(() =>
+                new Curso(_nome, cargaHorariaInvalida, _publicoAlvo, _valor));
+
+        }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmValorMenor1(double valorInvalido)
+        {
+;
+            Assert.Throws<ArgumentException>(() =>
+                new Curso(_nome, _cargaHoraria, _publicoAlvo, valorInvalido));
+        }
+
     }
     public enum PublicoAlvo
     {
@@ -59,6 +89,14 @@ namespace DomainTest.Curso
         public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
         {
             if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentException();
+            }
+            if(cargaHoraria < 1)
+            {
+                throw new ArgumentException();
+            }
+            if(valor < 1)
             {
                 throw new ArgumentException();
             }
